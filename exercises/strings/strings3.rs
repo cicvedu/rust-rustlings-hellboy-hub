3e0d73f83a4c1752f0e45f2161b502a8ecc13987
hellboy-hub <895482050@qq.com>
2023-08-13 22:03:45 +0800

    exercise fouth  commit

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,25 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut mid = input.to_string();
+    let modify =mid.trim();
+    modify.to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut modify = input.to_string();
+    modify.push_str(" world!");
+    modify.to_string()
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let  modify = input.to_string().replace("cars","balloons");
+    modify.to_string()
 }
 
 #[cfg(test)]
