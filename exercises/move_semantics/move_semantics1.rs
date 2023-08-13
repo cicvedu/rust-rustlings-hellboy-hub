f096665ac6ae5dbff408ea4ca9f0224b8756cfaf
hellboy-hub <895482050@qq.com>
2023-08-13 09:31:08 +0800

    exercise second time commit

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -7,7 +7,7 @@
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(& vec0);
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -16,12 +16,12 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+    let mut new_vec = vec.clone();
 
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
+    new_vec.push(22);
+    new_vec.push(44);
+    new_vec.push(66);
 
-    vec
+    new_vec
 }
